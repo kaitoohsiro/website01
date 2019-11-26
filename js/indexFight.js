608b59062c4eeb018f266a9e1d86fa18e58b109f
kaitooshiro <kaitooshiro@outlook.jp>
2019-11-26 23:50:18 +0900

    文言変更

---
+++ js/indexFight.js
@@ -0,0 +1,29 @@
+var Vue = new Vue({
+    el:'#app',
+    data:{
+        name01: {
+            name01: "red",
+            hp01: 500,
+            mp01: 200,
+            lv01: 10
+          },
+          name02: {
+            name02: "green",
+            hp02: 450,
+            mp02: 250,
+            lv02: 10
+          },
+          name03: {
+            name03: "pink",
+            hp03: 350,
+            mp03: 500,
+            lv03: 10
+          },
+          name04: {
+            name04: "black",
+            hp04: 400,
+            mp04: 400,
+            lv04: 10
+          },
+    }
+})
